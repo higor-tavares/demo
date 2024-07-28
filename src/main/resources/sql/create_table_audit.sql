57dbb446c6fdfb0dfaae5af9c17284f4c1e3f9e3
Higor Tavares <paulohtfreire@gmail.com>
2024-07-28 20:56:32 -0300

    generate tables

--- src/main/resources/sql/create_table_audit.sql
+++
@@ -1,8 +0,0 @@
-create table audit(
-    id serial not null,
-    username varchar(60) not null,
-    user_action varchar(20) not null,
-    created_at timestamp not null,
-    description varchar(255) not null,
-    ip varchar(64) not null
-)
--- src/main/resources/sql/create_table_users.sql
+++
@@ -1,9 +0,0 @@
-create table users(
-    id serial not null,
-    name varchar(60) not null,
-    email varchar(60) unique not null,
-    birthday timestamp not null,
-    created_at timestamp not null default now(),
-    deleted_at timestamp null,
-    is_deleted boolean default false
-)
